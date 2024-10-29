534fc5f2d3c1329df93876d37d02b97bc99540ce
KamilBorowiecki <kamil.borowiecki@op.pl>
2024-10-29 09:10:27 +0100

    fix

--- GaussianFilter/Program.cs
+++ GaussianFilter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -32,37 +33,68 @@ namespace GaussianFilter
 
         static unsafe void Main(string[] args)
         {
-            byte[] bitmapSource = new byte[]
+            String line;
+            try
             {
-                1, 2, 3, 0,  
-                4, 5, 6, 0,  
-                7, 8, 9, 0   
-            };
-
-            byte[] bitmapOutput = new byte[12]; 
-
-            Thread[] threads = new Thread[3];
-
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Users\\Kamil\\Desktop\\Projekt_JA_filtrGaussa\\GaussianFilter\\bitmapa.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
             {
-                for (int i = 0; i < 3; i++)
-                {
-                    int start = i * 4; 
-
-                    threads[i] = new Thread(() => PointerOnBitmapLine(bitmapSource, bitmapOutput, start));
-                    threads[i].Start();
-                }
-
-                foreach (Thread t in threads)
-                {
-                    t.Join();
-                }
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
             }
 
-            for (int i = 0; i < bitmapOutput.Length; i++)
-            {
-                Console.Write(bitmapOutput[i] + " ");
-                if ((i + 1) % 4 == 0) Console.WriteLine(); 
-            }
+
+
+            //byte[] bitmapSource = new byte[]
+            //{
+            //    1, 2, 3, 0,  
+            //    4, 5, 6, 0,  
+            //    7, 8, 9, 0   
+            //};
+
+            //byte[] bitmapOutput = new byte[12]; 
+
+            //Thread[] threads = new Thread[3];
+            //int threadsCount = bitmapSource.Length/4;
+
+            //{
+            //    for (int i = 0; i < threadsCount; i++)
+            //    {
+            //        int start = i * 4; 
+
+            //        threads[i] = new Thread(() => PointerOnBitmapLine(bitmapSource, bitmapOutput, start));
+            //        threads[i].Start();
+            //    }
+
+            //    foreach (Thread t in threads)
+            //    {
+            //        t.Join();
+            //    }
+            //}
+
+            //for (int i = 0; i < bitmapOutput.Length; i++)
+            //{
+            //    Console.Write(bitmapOutput[i] + " ");
+            //    if ((i + 1) % 4 == 0) Console.WriteLine(); 
+            //}
         }
     }
 }
